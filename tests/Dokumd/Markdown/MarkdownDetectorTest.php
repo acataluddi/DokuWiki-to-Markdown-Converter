edb985a640b0d10ad674df5b2360618ed77969d0
Adriao Cataluddi <acataluddi@gmail.com>
2024-01-28 12:15:56 +0100

    Refactored project for unit testing #2

---
+++ tests/Dokumd/Markdown/MarkdownDetectorTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) 2016 Metagûsto <info@metagusto.com>
+ */
+
+namespace Dokumd\Markdown;
+
+use PHPUnit\Framework\TestCase;
+
+class MarkdownDetectorTest extends TestCase
+{
+    public function testContainsMarkdown()
+    {
+        $this->assertTrue(true);
+    }
+}
